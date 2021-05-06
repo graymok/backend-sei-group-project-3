fe273d90c8340f7f5786588570a1bb5702d00c80
JaysonSovanHong <jaysonsovanhong@gmail.com>
2021-05-06 16:36:07 -0400

    cart routes and controller

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,4 +4,5 @@ const userController = require('../controllers/userController')
 userRoutes.post('/signup', userController.signup)
 userRoutes.post('/login', userController.login)
 userRoutes.get('/verify',userController.verify)
+
 module.exports = userRoutes
--- server.js
+++ server.js
@@ -8,13 +8,15 @@ const routesReport = require('rowdy-logger').begin(app)
 app.use(express.json())
 app.use(require('cors')())
 
-const userRoutes = require('./routes/userRoutes')
+const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 const orderRoutes = require('./routes/orderRoutes');
+const cartRoutes = require('./routes/cartRoutes');
 
 app.use('/user', userRoutes);
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/carts',cartRoutes);
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
