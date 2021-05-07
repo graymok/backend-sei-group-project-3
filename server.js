45f750a5fcc90e4136a9a89839386f1fad83d594
JaysonSovanHong <jaysonsovanhong@gmail.com>
2021-05-07 16:45:12 -0400

    sovanhong

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const app = express();
 
-app.use(require('morgan')('tiny'))
-const routesReport = require('rowdy-logger').begin(app)
+app.use(require("morgan")("tiny"));
+const routesReport = require("rowdy-logger").begin(app);
 
-app.use(express.json())
-app.use(require('cors')())
+app.use(express.json());
+app.use(require("cors")());
 
-const userRoutes = require('./routes/userRoutes');
-const productRoutes = require('./routes/productRoutes');
-const orderRoutes = require('./routes/orderRoutes');
-const cartRoutes = require('./routes/cartRoutes');
+const userRoutes = require("./routes/userRoutes");
+const productRoutes = require("./routes/productRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const cartRoutes = require("./routes/cartRoutes");
 
-app.use('/user', userRoutes);
-app.use('/products', productRoutes);
-app.use('/orders', orderRoutes);
-app.use('/cart',cartRoutes);
+app.use("/user", userRoutes);
+app.use("/products", productRoutes);
+app.use("/orders", orderRoutes);
+app.use("/cart", cartRoutes);
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`server listening on ${PORT}`);
-  routesReport.print()
-})
+  routesReport.print();
+});
